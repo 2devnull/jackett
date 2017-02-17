41dbaa9d4458f7260ed5ef04abb9782fe3d95b3d
2devnull <2devnull@users.noreply.github.com>
2017-02-17 16:44:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,6 @@ curl -L -o jackett.tar.gz https://github.com/Jackett/Jackett/releases/download/$
 tar xvf jackett.tar.gz -C /opt
 [[ -d /opt/Jackett ]] || mv /opt/Jackett* /opt/Jackett
 
-# install jackett-public
-ver=$(curl https://github.com/dreamcat4/Jackett-public/releases/latest | sed 's|.*tag/\([^"]\+\).*|\1|')
-curl -L -o jackett-public.tar.gz https://github.com/dreamcat4/Jackett-public/releases/download/${ver}/Jackett-public.Binaries.Mono.tar.gz
-tar xvf jackett-public.tar.gz -C /opt
-[[ -d /opt/Jackett-public ]] || mv /opt/Jackett-public* /opt/Jackett-public
-
 cd /
 rm -rf /build
 apt-get -y clean
