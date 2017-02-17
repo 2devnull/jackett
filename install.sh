eeb04dba5eb361dfea4572aa9bb5e0626168f42b
2devnull <2devnull@users.noreply.github.com>
2017-02-17 16:54:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@ set -e
 
 # use US mirror
 sed -i 's/httpredir/ftp.us/g' /etc/apt/sources.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
+apt-get update
 echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://download.mono-project.com/repo/debian wheezy-libjpeg62-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install mono-complete
+apt-get install mono-complete
 apt-get -y upgrade
-apt-get -y install curl libmono-cil-dev supervisor
+apt-get -y install curl nano supervisor
 
 mkdir -p /build
 cd /build
